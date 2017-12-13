219db058d2d04506a5d85630208d08090304d283
ezio84 <brabus84@gmail.com>
2017-12-13 11:17:55 +0100

    Fix custom lockscreen shortcut icons being white
    now 8.1 sets a tint to make icons black and white according to the
    wallpaper main color but this is not suitable for custom icons
    let's fix this setting the tint only for default lockscreen buttons
    
    Change-Id: I79c39fb39e308f9815afda0c85fcd3922c528c69

--- packages/SystemUI/src/com/android/systemui/tuner/LockscreenFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/LockscreenFragment.java
@@ -354,6 +354,8 @@ public class LockscreenFragment extends PreferenceFragment {
         @Override
         public IntentButton create(Map<String, String> settings) {
             String buttonStr = settings.get(mKey);
+            // if buttonStr is not empty it means it's not the Default button,
+            // so we'll create a custom one
             if (!TextUtils.isEmpty(buttonStr)) {
                 if (buttonStr.contains("::")) {
                     return new ShortcutButton(mContext, buttonStr);
@@ -386,6 +388,7 @@ public class LockscreenFragment extends PreferenceFragment {
             mIconState.drawable = new ScalingDrawableWrapper(mIconState.drawable,
                     mSize / (float) mIconState.drawable.getIntrinsicWidth());
             mIconState.tint = false;
+            mIconState.isDefaultButton = false;
             init();
         }
 
@@ -441,6 +444,7 @@ public class LockscreenFragment extends PreferenceFragment {
             mIconState.drawable = new ScalingDrawableWrapper(mIconState.drawable,
                     mSize / (float) mIconState.drawable.getIntrinsicWidth());
             mIconState.tint = false;
+            mIconState.isDefaultButton = false;
             init();
         }
 
@@ -499,6 +503,7 @@ public class LockscreenFragment extends PreferenceFragment {
         public HiddenButton() {
             mIconState = new IconState();
             mIconState.isVisible = false;
+            mIconState.isDefaultButton = false;
         }
 
         @Override
