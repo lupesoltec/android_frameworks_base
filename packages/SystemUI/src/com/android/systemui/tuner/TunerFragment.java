78a12d8c0377c56c184f2d1e296247be0c23a57d
Ritesh Saxena <riteshsax007@gmail.com>
2018-12-14 06:54:56 +0000

    tuner: Remove the warning and setting to remove the tuner
    Change-Id: Ib51fe7f98872c4045708cefeaf71c08a27a3960c

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -83,12 +83,12 @@ public class TunerFragment extends PreferenceFragment {
             }
         }
 
-        if (Settings.Secure.getInt(getContext().getContentResolver(), SETTING_SEEN_TUNER_WARNING,
+        /*if (Settings.Secure.getInt(getContext().getContentResolver(), SETTING_SEEN_TUNER_WARNING,
                 0) == 0) {
             if (getFragmentManager().findFragmentByTag(WARNING_TAG) == null) {
                 new TunerWarningFragment().show(getFragmentManager(), WARNING_TAG);
             }
-        }
+        }*/
     }
 
     private boolean alwaysOnAvailable() {
@@ -112,7 +112,7 @@ public class TunerFragment extends PreferenceFragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(Menu.NONE, MENU_REMOVE, Menu.NONE, R.string.remove_from_settings);
+        //menu.add(Menu.NONE, MENU_REMOVE, Menu.NONE, R.string.remove_from_settings);
     }
 
     @Override
@@ -121,7 +121,7 @@ public class TunerFragment extends PreferenceFragment {
             case android.R.id.home:
                 getActivity().finish();
                 return true;
-            case MENU_REMOVE:
+            /*case MENU_REMOVE:
                 TunerService.showResetRequest(getContext(), new Runnable() {
                     @Override
                     public void run() {
@@ -130,7 +130,7 @@ public class TunerFragment extends PreferenceFragment {
                         }
                     }
                 });
-                return true;
+                return true;*/
         }
         return super.onOptionsItemSelected(item);
     }
