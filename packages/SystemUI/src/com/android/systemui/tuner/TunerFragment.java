394441802c81191b927d882780c46ecaa98dc317
Ritesh Saxena <riteshsax007@gmail.com>
2019-01-13 04:50:35 +0000

    tuner: Unlock navbar and lockscreen options
    Change-Id: Icf6f9efd659b9ea5e74ee9da2a161a37224c0e10

--- packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerFragment.java
@@ -47,8 +47,6 @@ public class TunerFragment extends PreferenceFragment {
 
     private static final String WARNING_TAG = "tuner_warning";
     private static final String[] DEBUG_ONLY = new String[] {
-            "nav_bar",
-            "lockscreen",
             "picture_in_picture",
     };
 
